55e007e5de0ce378ee736605fd6a339391bcddc3
prabheen09876 <prabheen09876@gmail.com>
2024-01-01 00:32:53 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import React, { useState } from 'react';
+
+
+function App() {
+
+ 
+  return (
+    <div className="w-full h-screen bg-zinc-800">
+<div className='absolute top-[5%] w-full py-10 flex justify-center text-zinc-600 text-xls font-semibold'>Document</div>
+
+
+<h1 className='absolute top-1/2 left-1/2 -translate-x-[50%] -translate-y-[50%]  text-[13vw] leading-none tracking-tighter text-zinc-900'>DOCS.</h1>
+    </div>
+    
+  )
+}
+
+export default App
---
+++ src/Products.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Products() {
+  return (
+    <div className="text-white">
+    
+    </div>
+  )
+}
+
+export default Products
